f3b618e15689cd013e80d67c76b72009ed9ddfd6
Ali-johnno <ajay051701@gmail.com>
2020-10-06 23:19:03 -0500

    Updated tic-tac-toe.js Exercise 2

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -3,4 +3,45 @@ document.addEventListener("DOMContentLoaded", function(){
     for (var i=0; i<9; i++){
         divs.getElementsByTagName('div')[i].classList.add("square");
     }
+
+    boardStats = [
+        '', '','',
+        '', '', '',
+        '', '', ''
+    ];
+
+    const X_Class = 'X';
+    const O_Class = 'O';
+    const gridCells = document.querySelector('#board').children;
+    var index = 0;
+
+    xTurn = X_Class;
+
+    for (var i=0; i<gridCells.length;i++){
+        gridCells[i].addEventListener('click', handleClick, {once:true});
+    }
+
+    function handleClick(e){
+        const gridCell = e.target;
+        const currentClass = xTurn ? X_Class : O_Class;
+        placeXoO (gridCell, currentClass);
+        index ++;
+        nextTurn();
+    }
+
+    function placeXoO(gridCell,currentClass){
+        if (currentClass == X_Class){
+            gridCell.innerHTML = 'X';
+            boardStats[index] = 'X';
+        } else {
+            gridCell.innerHTML = 'O';
+            boardStats[index] = 'O';
+        }
+        gridCell.classList.add(currentClass);
+    }
+
+    function nextTurn (){
+        xTurn = !xTurn;
+    }
 });
+

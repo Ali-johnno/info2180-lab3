6ba3a6549e5c6656ff06cc7beeebda4d510aebf8
Ali-johnno <ajay051701@gmail.com>
2020-10-11 13:01:30 -0500

    Final changes and comments

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,10 +1,12 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){ 
     var divs = document.getElementById("board");
     for (var i=0; i<9; i++){
-        divs.getElementsByTagName('div')[i].classList.add("square");
+        divs.getElementsByTagName('div')[i].classList.add("square"); //applies the css square class to the html div children of div id = "board"
     }
 
+    //empty array to check the status of the game.
+    //This array will be used to check for winners
     var boardStats = [
         '', '','',
         '', '', '',
@@ -15,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const O_Class = 'O';
     const gridCells = document.querySelector('#board').children;
     const newGame = document.getElementsByClassName('btn');
-    var isFull = 0;
+    var isFull = 0; //checks to see if board is full to determine game draw
     var winner = '';
 
     var xTurn = X_Class;
@@ -24,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function(){
     
     startGame();
     
+    //The state of the tic-tac-toe game is set to its original state
     function startGame(){
         setHover();
         winner = '';
@@ -44,7 +47,10 @@ document.addEventListener("DOMContentLoaded", function(){
         document.getElementById("status").innerHTML = "Move your mouse over a square and click to play an X or an O.";
     }
     
-
+    /**
+     * when a tic-tac-toe box is presses it sets the hover, places X  or O, switches turns and checks for winner
+     * @param {*} e 
+     */
     function handleClick(e){
         if (winner == ''){
             const gridCell = e.target;
@@ -57,7 +63,11 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
-    function placeXoO(gridCell,currentClass, e){
+    /**
+     * The class X or O is placed respectively in the boxes
+     * The array index of the box the user clicks and the class is stored into the same index of the board status array 
+     */
+    function placeXoO(gridCell,currentClass){
         var squareIndexes = Array.prototype.slice.call(gridCells); 
         var squareIndex = squareIndexes.indexOf(gridCell);
         if (currentClass == X_Class){
@@ -70,10 +80,16 @@ document.addEventListener("DOMContentLoaded", function(){
         gridCell.classList.add(currentClass);
     }
 
+    /**
+     * Swaps turns for classes X or O
+     */
     function nextTurn (){
         xTurn = !xTurn;
     }
 
+    /**
+     * Sets a hover whenever the mouse is over a tic-tac-toe box
+     */
     function setHover(){
         for (var i=0; i<gridCells.length;i++){
             gridCells[i].addEventListener("mouseover", hover);
@@ -81,16 +97,27 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+    /**
+     * Adds hover class to the cell that the mouse is over
+     */
     function hover(e){
         e.target.classList.add('hover');
     }
 
+    /**
+     * removes hover when the mouse is no longer over that cell
+     */
     function hoverFalse(e){
         const targ = e.target;
         targ.classList.remove("hover");
-        targ.classList.add("square");
+        /*targ.classList.add("square");*/
     }
 
+   /**
+    * The possible winning combination indexes are checked to see if they have the same class
+    * If they do the current player is determined the winner
+    * @param {*} currentClass 
+    */
     function isWinner(currentClass){
         if (boardStats[0] == 'X' && boardStats[1] == 'X' && boardStats[2] == 'X' || boardStats[0] == 'O'&& boardStats[1] == 'O' && boardStats[2] == 'O'){
             winner = currentClass;
@@ -116,8 +143,14 @@ document.addEventListener("DOMContentLoaded", function(){
         setWinnerText(winner);
     }
 
+    /**
+     * The Congratulations message is set depending on the winner with "you-won" css class attached to it
+     * if there is no winner draw is displayed
+     * @param {*} winner 
+     */
     function setWinnerText (winner){
         var statusText= document.getElementById("status");
+
         if (winner == X_Class){
             statusText.classList.add("you-won");
             statusText.innerHTML = "Congratulations! X is the Winner!";
@@ -128,7 +161,5 @@ document.addEventListener("DOMContentLoaded", function(){
             statusText.innerHTML = "DRAW";
         }
     }
-
-
 });
 
